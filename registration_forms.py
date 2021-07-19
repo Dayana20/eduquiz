df4c3b64afcbb0ef79f047349960caf60d21545b
Anthony Meza <abryantmeza@gmail.com>
2021-07-19 19:01:50 +0000

    Testing commit - anthony meza

--- registration_forms.py
+++ registration_forms.py
@@ -9,4 +9,4 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    
+
