6a15e3145b875e584e2b216f855e361ae9ef31af
Anthony Meza <abryantmeza@gmail.com>
2021-07-19 19:30:34 +0000

    Added login form in registration_forms.py, and also added login function in scrypt.py so that a login page can now be processed

--- script.py
+++ script.py
@@ -7,7 +7,6 @@ proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'e6d894ab9aa5e4b585f29ff83a43d75c'
 
 @app.route("/")
-
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -15,6 +14,17 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+  
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        flash(f'{form.username.data} successfully logged in!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
 
+  
+  
+  
 if __name__ == '__main__':
   app.run(debug=True, host="0.0.0.0")
