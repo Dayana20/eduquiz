3c71963c399bd7284b90ee9531458328cf1bd61d
Chris Berniel Cobashatse <cc4536@columbia.edu>
2021-07-19 22:49:19 +0000

    implemented code to create database to store results from API calls to Open Trivia D8

--- apis.py
+++ apis.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 
 
 def get_data_from_api(base_url):
-    
+
     response = requests.get(base_url)
     response_json = response.json()
     results = response_json['results']
@@ -21,8 +21,15 @@ def get_data_items(results):
     difficulty = results['difficulty']
     question = results['question']
     correct_answer = results['correct_answer']
-    incorrect_answers = results['incorrect_answers']  # this is a list
-    return category, question_type, difficulty, question, correct_answer, incorrect_answers
+    incorrect_answers_list = results['incorrect_answers']  # this is a list
+    if len(incorrect_answers_list) == 1:
+        incorrect_answers = incorrect_answers_list[0]
+    else:
+        incorrect_answers = incorrect_answers_list[0] + ', ' +\
+            incorrect_answers_list[1] + ', ' +\
+            incorrect_answers_list[2]
+    return category, question_type, difficulty, question, correct_answer,\
+        incorrect_answers
 
 
 def create_dataframe():
@@ -45,15 +52,11 @@ def create_engine_function(dbName):
 
 def save_data_to_file(dtfr, dbName, tableName, fileName):
     dtfr.to_sql(tableName, con=create_engine_function(dbName),
-                if_exists='replace',
-                index=False)
+                if_exists='replace', index=False)
     os.system('mysqldump -u root -pcodio {} > {}.sql'.format(dbName, fileName))
 
 
 def load_database(dbName, fileName):
-    # os.system('mysql -u root -pcodio -e
-    # "CREATE DATABASE IF NOT EXISTS covid_data.sql;"')
-    # os.system("mysql -u root -pcodio covid < covid_data.sql")
     os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '
               + dbName + '; "')
     os.system('mysql -u root -pcodio ' + dbName + ' < ' + fileName + '.sql')
@@ -66,11 +69,26 @@ def main():
     dbName = 'quiz_db'
 
     base_url = 'https://opentdb.com/api.php?amount=2'
+
     # explore the session_token option to get unique questions
     # session_token = '12286e9bb10cfcfa9ea7597f1ee68cdd'
+
+    load_database(dbName, fileName)
+    dtfr_initial = pd.read_sql_table(tableName,
+                                     con=create_engine_function(dbName))
+
     results = get_data_from_api(base_url)
-    values = get_data_items(results[0])
-    print(values)
+    for question in results:
+        values = get_data_items(question)
+        # print(values)
+
+        # create a new dataframe/database/SQL file
+        # dataframe = create_dataframe()
+
+        dtfr_final = put_values_dataframe(dtfr_initial, values)
+        save_data_to_file(dtfr_final, dbName, tableName, fileName)
+
+    print(dtfr_final.tail(1))
 
 
 if __name__ == "__main__":
