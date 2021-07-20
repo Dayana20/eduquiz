ae74dde13e6c0180f30534d2f679e651ba3c7bc3
Anthony Meza <abryantmeza@gmail.com>
2021-07-20 15:07:19 +0000

    Finished implementing registration feature, login feature, encryption of passwords, as well as verification of proper inputs for registration (not giving repeated values) and checking login is a valid login

--- script.py
+++ script.py
@@ -4,12 +4,18 @@ from flask_behind_proxy import FlaskBehindProxy
 from flask_sqlalchemy import SQLAlchemy
 import secrets
 
+from flask_bcrypt import Bcrypt #install flask-bcrypt
 
 app = Flask(__name__)
 proxied = FlaskBehindProxy(app)  
 #print(secrets.token_hex(16))
 app.config['SECRET_KEY'] = '0a85f9ea1879f713046952c8db9a1d6a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+
+# Set up bcrypt for password hashing
+bcrypt = Bcrypt()
 
 # For storing user data
 class User(db.Model):
@@ -22,10 +28,41 @@ class User(db.Model):
     return f"User('{self.username}', '{self.email}', '{self.password}')"
 
 
+# Give a password to encrpyt by salting and hashing
+def encrypt_password(password):
+    return bcrypt.generate_password_hash(password)
+       
+
+# Check if encrypted pasword and guess input password match, thus valid
+def check_password_match(pw_hash, guess):
+    return bcrypt.check_password_hash(pw_hash, guess) 
+
+  
+  
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit(): # checks if entries are valid
+        
+        # Check database if username is already in use
+        exist_user = User.query.filter_by(username=form.username.data).first()
+        if exist_user is not None:
+            flash(f'Username {exist_user.username} is already taken', 'danger')
+            return render_template('register.html', title='Register', form=form)
+        
+        # Check database if email is already in use
+        exist_user = User.query.filter_by(email=form.email.data).first()
+        if exist_user is not None:
+            flash(f'Email {exist_user.email} is already taken', 'danger')
+            return render_template('register.html', title='Register', form=form)
+        
+        # User can be registered
+        user = User(username=form.username.data, 
+                    email=form.email.data, 
+                    password= encrypt_password(form.password.data))
+        db.session.add(user)
+        db.session.commit()
+        
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('login')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
@@ -35,13 +72,26 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit(): # checks if entries are valid
+        login_user = User.query.filter_by(username=form.username.data).first()
+        
+        # Username does not exist
+        if login_user is None:
+            flash(f'Username {form.username.data} does not exist!', 'danger')
+            return render_template('login.html', title='Login', form=form)
+        
+        #Incorrect Password
+        if not check_password_match(login_user.password, form.password.data):
+            flash(f'Incorrect password ', 'danger')
+            return render_template('login.html', title='Login', form=form)
+        
+        # Successful Login
         flash(f'{form.username.data} successfully logged in!', 'success')
         return redirect(url_for('home')) # if so - send to home page
+        
     return render_template('login.html', title='Login', form=form)
 
 
 # Temp function so it can exist without error
-
 @app.route("/")
 @app.route("/home")
 def home():
