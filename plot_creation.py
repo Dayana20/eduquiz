a4bf8beb1bd658741b7631c2a3461920d938abb4
Anthony Meza <abryantmeza@gmail.com>
2021-07-21 17:58:19 +0000

    Created file plot_creation that will be housing some functions that will allow us to create html string to print into our weebpages in place of src inputs. Created a base html converter script, and also a basic scatter plot script.

---
+++ plot_creation.py
@@ -0,0 +1,54 @@
+####
+# plot_creation.py
+# 
+# Functions will be in charge of creating plots using matplotlib
+# and then converting them into html string so that they can be 
+# posted onto the webpages as images
+###
+import pandas as pd
+import matplotlib
+import matplotlib.figure as figure
+import base64
+from io import BytesIO
+
+# Function creates html string given a figure.Figure() object
+# @para fig:
+# @return: html string that will be used to load the image
+#          this string can be inserted in src for html script. Ex:
+#         <img src='HTML_STRING'/>
+def create_html_string(fig):
+  html_string = ""
+  
+  buf = BytesIO()
+  fig.savefig(buf, format="png")
+  
+  # Embed the result in the html output
+  data = base64.b64encode(buf.getbuffer()).decode("ascii")
+  html_string = f'data:image/png;base64,{data}'
+  
+  #DEBUG
+  print(html_string)
+  
+  return html_string
+
+
+
+# Creates scatter plot that compares x and y axis parameters
+# @para     x: array of x-axis values
+# @para   x_l: string of x-axis value
+# @para     y: array of y-axis values
+# @para   y_l: string of y-axis value
+# @para title: string title of plot
+# @return: html string that will load the image (refer to create_html_string()
+#          function for more details)
+def create_scatter_html(x, x_l, y, y_l, title):
+    fig = figure.Figure()
+    scatter = fig.add_subplot()
+    scatter.scatter(x=x, y=y)
+    scatter.set_xlabel(x_l)
+    scatter.set_ylabel(y_l)
+    scatter.set_title(title)
+    
+    # Create and return html string
+    
+    return create_html_string(fig)
